c749eaaa05f2d7071d55ac5f8f42dccebf2c6c0c
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-24 17:55:30 +0800

    chore: added `id` on @Param() type annotations

--- api/src/todo/todo.controller.ts
+++ api/src/todo/todo.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo } from './schemas/todo.schema';
@@ -6,35 +14,35 @@ import { TodoService } from './todo.service';
 
 @Controller('todos')
 export class TodoController {
-  constructor(private service: TodoService) { }
+  constructor(private service: TodoService) {}
 
   @Get()
   async findAll(): Promise<Todo[]> {
-    return await this.service.findAll()
+    return await this.service.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param() id: string) {
-    return await this.service.findOne(id)
+  async findOne(@Param('id') id: string) {
+    return await this.service.findOne(id);
   }
 
   @Post()
   async create(@Body() createTodoDto: CreateTodoDto) {
-    return await this.service.create(createTodoDto)
+    return await this.service.create(createTodoDto);
   }
 
   @Put(':id')
-  async update(@Param() id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return await this.service.update(id, updateTodoDto)
+  async update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return await this.service.update(id, updateTodoDto);
   }
 
   @Delete(':id')
-  async delete(@Param() id: string) {
-    return await this.service.delete(id)
+  async delete(@Param('id') id: string) {
+    return await this.service.delete(id);
   }
 
   @Delete(':id')
-  async forceDelete(@Param() id: string) {
-    return await this.service.deleteForever(id)
+  async forceDelete(@Param('id') id: string) {
+    return await this.service.deleteForever(id);
   }
 }
