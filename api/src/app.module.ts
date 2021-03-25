2e48941b6f1b103373280665b36c5b9afbc28a15
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-25 09:24:50 +0800

    feat: added new modules into `AppModule`

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,12 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-todo'),
-    TodoModule],
+    TodoModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
