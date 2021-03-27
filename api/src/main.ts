2b8fc4af9df5ff51b87182d26acd11c87c17406d
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-27 19:03:10 +0800

    ...

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors();
+
+  // all routes prefixed with '/api'
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  
+  await app.listen(parseInt(process.env.APP_PORT, 10) || 3000);
+  console.log(
+    `[App] App server started, listening at port ${process.env.APP_PORT}`,
+  );
 }
 bootstrap();
